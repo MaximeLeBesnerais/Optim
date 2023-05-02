998a067a0ae69c0cd25d9d4bd269c3a1bb0de85d
Maxime <maxime.le-besnerais@epitech.eu>
2023-05-02 16:34:25 +0200

    [STRUCTS & GLOBALS] Moves include in graph & creates globals in this file 	Files: 		include/graph.hpp: Modified
    	Comment:
    		Commit made without comment

--- include/graph.hpp
+++ include/graph.hpp
@@ -14,5 +14,27 @@
     #include <nana/gui/widgets/combox.hpp>
     #include <nana/gui/widgets/form.hpp>
     #include <nana/paint/image.hpp>
-    int g_main(void);
+    #include <nana/gui/widgets/button.hpp>
+    #define WINDOW_WIDTH 500
+    #define WINDOW_HEIGHT 150
+    typedef enum GPU_STATUS {
+        INTEGRATED = 0,
+        HYBRID = 1,
+        NVIDIA = 2
+    } gpu;
+    typedef struct buttons_s {
+        nana::button &integrated;
+        nana::button &hybrid;
+        nana::button &nvidia;
+        nana::button &apply;
+    } buttons_t;
+    typedef struct labels_s {
+        nana::label &current;
+        nana::label &next;
+        nana::label &start;
+        nana::label &changes;
+    } labels_t;
+    gpu get_current(void);
+    static gpu c_gpu = get_current();
+    static gpu a_gpu = get_current();
 #endif //GRAPH_HPP
